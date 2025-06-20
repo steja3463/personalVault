297bc333bf8149ee5241623370bd621a9b850818
steja3463 <steja3463@gmail.com>
2025-06-20 15:49:35 +0530

    Update auth.js

--- digitalLocker/routes/auth.js
+++ digitalLocker/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../Controller/authController');
 const auth = require('../middleware/auth');
-
+//router
 
 router.post('/register', authController.register);
 
